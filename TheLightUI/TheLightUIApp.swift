eb4a6b91618c20b81c6eabd5da85253a76c5eaf2
Djanibek Khudaybergenov <djanibek.k@gmail.com>
2020-12-09 10:51:30 +0300

    Initial Commit

---
+++ TheLightUI/TheLightUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheLightUIApp.swift
+//  TheLightUI
+//
+//  Created by D K on 09.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct TheLightUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
